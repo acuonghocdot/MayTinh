d4ea8a22d4a4d5fa13329bb0f406ae35647b3e71
Le Nhat Tung <hoanghuuhanh321@gmail.com>
2023-09-09 12:43:12 +0700

    New

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+const obj2 = arrToObj([
+//     ['name', 'Duc Long'],
+//     ['age', 18],
+//     ['address', 'Ha Noi']
+// ])
+// console.log(obj2)
+// // Output: { name: 'Duc Long', age: 18, address: 'Ha Noi' }
+// // //
